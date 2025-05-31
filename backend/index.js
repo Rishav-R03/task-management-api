f6a0fae3bd0d3e7dc475cce1254bed395dd7d2e2
Rishav-R03 <rishav042023@gmail.com>
2025-05-31 10:13:26 +0530

    database connection fix, and contoller contact added

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import getById from './routes/getById.route.js'
 import updateByIdRoute from './routes/updateById.route.js';
 import deleteByIdRoute from './routes/deleteById.route.js';
 import cookieParser from 'cookie-parser'
-import logoutRouter from './routes/logout.router.js'; 
+import logoutRouter from './routes/logout.router.js';
 import homepageRouter from './routes/homepage.route.js'
 
 // application 
@@ -20,24 +20,24 @@ const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
 dotenv.config()
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 //connected database 
 conn();
 //port to host incoming traffic
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT;
 // api end points
 // signup route 
-app.use('/api/v1',homepageRouter);
-app.use('/api/v1/signup',singupRoute);
-app.use('/api/v1/addTask',addTaskRoute);
-app.use('/api/v1/login',loginRoute);
-app.use('/api/v1/getAllTodo',getAllRoute);
-app.use('/api/v1/getById',getById); 
-app.use('/api/v1/updateById',updateByIdRoute); 
-app.use('/api/v1/deleteById',deleteByIdRoute); 
-app.use('/api/v1/logout',logoutRouter);
+app.use('/api/v1', homepageRouter);
+app.use('/api/v1/signup', singupRoute);
+app.use('/api/v1/addTask', addTaskRoute);
+app.use('/api/v1/login', loginRoute);
+app.use('/api/v1/getAllTodo', getAllRoute);
+app.use('/api/v1/getById', getById);
+app.use('/api/v1/updateById', updateByIdRoute);
+app.use('/api/v1/deleteById', deleteByIdRoute);
+app.use('/api/v1/logout', logoutRouter);
 
-app.listen(PORT,'0.0.0.0',()=>{
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running at http://localhost:8000/api/v1`)
 })
